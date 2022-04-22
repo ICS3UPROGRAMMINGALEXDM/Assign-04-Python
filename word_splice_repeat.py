a4eb1b7de7b77079562d831690d61330a9f93fb2
Ubuntu <ubuntu@ip-172-31-17-170.ec2.internal>
2022-04-22 17:10:21 +0000

    started

---
+++ word_splice_repeat.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created By: Alex De Meo
+# Date: 03/25/2022
+# Description: Gets a string from the user, then asks how long they want the
+# sub strings to be, along with how many times this sub string is repeated.
+# The program will then loop through according to the users "order" and display
+# The final string.
+
+
+
+
+def main():
+    u_string = input("Enter a word or phrase to be splice and diced: ")
+
+
+    u_size = input(
+        "Enter how many characters long you want the spliced word to be"
+    )
+    u_copies = input(
+        "Enter how many times you want the spliced word to repeat:"
+    )
+
+
+
+
+if __name__ == "__main__":
+    main()
