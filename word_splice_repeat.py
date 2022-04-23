ce324e452c9aa706f989ca7a36597a475cec8033
Ubuntu <ubuntu@ip-172-31-17-170.ec2.internal>
2022-04-23 03:36:50 +0000

    Added the function that splits characters into a list. Basic looping complete. Needs comments>

--- word_splice_repeat.py
+++ word_splice_repeat.py
@@ -7,20 +7,48 @@
 # The program will then loop through according to the users "order" and display
 # The final string.
 
+# function is used to split at each character, then input into an array.
+# takes the argument string
+def split_string(string):
+    return[char for char in string]
 
 
 
 def main():
-    u_string = input("Enter a word or phrase to be splice and diced: ")
+    restartLoop = True
 
+    while restartLoop:
+        answerLoop = True
+        errorLoop = True
 
-    u_size = input(
-        "Enter how many characters long you want the spliced word to be"
-    )
-    u_copies = input(
-        "Enter how many times you want the spliced word to repeat:"
-    )
+        u_string = input("Enter a word or phrase to be splice and diced: ")
+        # This calls the function that will
+        charArr = split_string(u_string)
 
+        while errorLoop:
+            u_size = input(
+                "Enter how many characters long you want the spliced word to be: "
+            )
+            u_copies = input(
+                "Enter how many times you want the spliced word to repeat: "
+            )
+
+            try:
+                size = int(u_size)
+                copies = int(u_copies)
+                if (copies < 1) or (size < 1) or (size > len(charArr)):
+                    print(
+                        "Make sure copies and size are greater than 0. AND that "
+                        +"size is NOT greater than the amout of characters in the"
+                        +" word/sentence")
+                else:
+                    errorLoop = False
+                    print(charArr)
+
+            except ValueError:
+                print(
+                    "Please ensure that the size and number of"
+                    + " copies entered is a valid number")
 
 
 
